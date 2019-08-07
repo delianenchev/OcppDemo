8129ab9ee55dc252e3aaf6da8686519eab6756c3
Delian Enchev <d.enchev@nrjsoft.com>
2019-08-07 10:37:07 +0300

    OCPP in WCF

---
+++ WcfInIISWithOcpp/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WcfInIISWithOcpp
+{
+    public class Class1
+    {
+        public void testc()
+        {
+            var item = new CentralSystemService();
+        }
+    }
+}
